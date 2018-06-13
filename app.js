8d94e0ee652d4ed90b25deb9f9d311172f6e9e04
hikaru-taka <hikaru@couger.co.jp>
2018-06-13 19:14:37 +0900

    add websocket

---
+++ app.js
@@ -0,0 +1,50 @@
+# https://github.com/theturtle32/WebSocket-Node
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+wsServer = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: false
+});
+
+wsServer.on('request', function(request) {
+    var connection = request.accept('berlin-hackathon', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.sendUTF('hello');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            try {
+                var command = JSON.parse(message.utf8Data);
+                if (command.cmd === 'recieve') {
+                    connection.sendUTF(message.utf8Data);
+                }
+            }
+            catch(e) {
+                // do nothing if there's an error.
+            }
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
+
