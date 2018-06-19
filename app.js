d8dbd3ad338ffe6a8180fe142749b4597ee0d312
Takanori Hirano <hrn.takanori@gmail.com>
2018-06-19 12:37:54 +0900

    implement protocol buffers

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 // original script from  https://github.com/theturtle32/WebSocket-Node
 const WebSocketServer = require('websocket').server;
 const http = require('http');
+const protobuf = require('protocol-buffers');
+const path = require('path');
+const fs = require('fs');
+
+const judgementAPIFile = fs.readFileSync(path.resolve(__dirname, './proto/judgement_api.proto'));
+const judgementAPI = protobuf(judgementAPIFile);
 
 const server = http.createServer(function(request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
@@ -27,23 +33,42 @@ wsServer.on('request', function(request) {
     console.log((new Date()) + ' Connection accepted.');
     connection.sendUTF('hello');
     connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            try {
-                const command = JSON.parse(message.utf8Data);
-                if (command.cmd === 'recieve') {
-                    connection.sendUTF(message.utf8Data);
-                }
-            } catch (e) {
-                // do nothing if there's an error.
-            }
-        } else if (message.type === 'binary') {
+        if (message.type === 'binary') {
             console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
+            judgement(message.binaryData);
+        } else {
+            console.log('Unsupported message type: ' + message.type);
         }
     });
     connection.on('close', function(reasonCode, description) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
+
+    // setInterval(()=>{
+    //     const res = judgementAPI.JudgementResponse.encode({
+    //         actions: [{
+    //             type: judgementAPI.ActionType.LookAt,
+    //             args: ['Camera'],
+    //         }],
+    //     });
+    //     connection.sendBytes(res);
+    // }, 1000);
 });
 
+/**
+ * judgement
+ * @param {Buffer} req
+ * @param {any} connection
+ */
+function judgement(req, connection) {
+    judgementAPI.JudgementRequest.decode(req);
+    console.log(req);
+
+    const res = judgementAPI.JudgementResponse.encode({
+        actions: [{
+            type: judgementAPI.ActionType.LookAt,
+            args: ['Camera'],
+        }],
+    });
+    connection.sendBytes(res);
+}
