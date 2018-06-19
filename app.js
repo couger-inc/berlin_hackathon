885205bd1984f2ab9f1296734b88005691792c5a
Takanori Hirano <hrn.takanori@gmail.com>
2018-06-19 12:36:38 +0900

    change listen port to 8888

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const server = http.createServer(function(request, response) {
     response.writeHead(404);
     response.end();
 });
-server.listen(8080, function() {
+server.listen(8888, function() {
     console.log((new Date()) + ' Server is listening on port 8080');
 });
 
